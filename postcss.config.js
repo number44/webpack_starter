c241ba3ebe776010b613005db77670f189c4e626
number44 <eldan8600@gmail.com>
2021-05-03 21:56:14 +0200

    grid support

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,8 @@ module.exports = {
       require('postcss-preset-env')({
         browsers: 'last 2 versions',
       }),
-      require("autoprefixer")
+      require("autoprefixer")({
+          grid : true
+      })
     ],
   };
